bc8539ad78011fd66a8198d6b2651d70223daa9b
AntoC <antonin.coupanec@gmail.com>
2024-09-19 21:50:17 +0200

    ci: add heap profiling

--- src/gameOfLife.cpp
+++ src/gameOfLife.cpp
@@ -1,8 +1,12 @@
 #include "InputChecker.hpp"
 #include "Game/Game.hpp"
 
-#ifdef ENABLE_PROFILING
+#ifdef ENABLE_CPU_PROFILING
 #include <gperftools/profiler.h>
+#endif
+
+#ifdef ENABLE_HEAP_PROFILING
+#include <gperftools/heap-profiler.h>
 #endif
 
 using namespace std;
@@ -10,8 +14,12 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 
-#ifdef ENABLE_PROFILING
-    ProfilerStart("profiler_output.pprof");
+#ifdef ENABLE_CPU_PROFILING
+    ProfilerStart("cpu_profiling.prof");
+#endif
+
+#ifdef ENABLE_HEAP_PROFILING
+    HeapProfilerStart("heap_profiling.prof");
 #endif
 
     static InputChecker checker;
@@ -25,8 +33,13 @@ int main(int argc, char *argv[])
     static Game gameOfLife(filePath, nbOfIterations, outputAllIterations);
 
     const bool processReturn = gameOfLife.process();
-#ifdef ENABLE_PROFILING
-    ProfilerStop(); // Stop the profiler
+#ifdef ENABLE_CPU_PROFILING
+    ProfilerStop(); 
+#endif
+
+#ifdef ENABLE_HEAP_PROFILING
+    HeapProfilerDump("Heap profiling") ;
+    HeapProfilerStop();
 #endif
 
     // True==1 but 1 is an error return so return the opposite of the return
