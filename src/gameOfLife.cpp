ea0763b2d17658be48f86ae6270d7a86129022fe
AntoC <129602681+Anto-dev-perso@users.noreply.github.com>
2024-09-19 22:10:26 +0200

    Create the CI of the repository (#1)
    ci: create the ci with github actions
    
    Squash commits of the following:
    * ci: add the first jobs of the ci
    
    * ci: keep binaries as artifacts
    
    * ci: add a test result report and static analyzis with codeQL
    
    * ci: use the codeql config file for manual build
    
    * ci: add coverage data and clang-tidy for static analyzis
    
    * ci: use gperftool for cpu profiling
    
    * ci: add heap profiling

--- src/gameOfLife.cpp
+++ src/gameOfLife.cpp
@@ -1,17 +1,47 @@
 #include "InputChecker.hpp"
 #include "Game/Game.hpp"
 
+#ifdef ENABLE_CPU_PROFILING
+#include <gperftools/profiler.h>
+#endif
+
+#ifdef ENABLE_HEAP_PROFILING
+#include <gperftools/heap-profiler.h>
+#endif
+
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+
+#ifdef ENABLE_CPU_PROFILING
+    ProfilerStart("cpu_profiling.prof");
+#endif
+
+#ifdef ENABLE_HEAP_PROFILING
+    HeapProfilerStart("heap_profiling.prof");
+#endif
 
     static InputChecker checker;
     const auto [inputsValids, filePath, nbOfIterations, outputAllIterations] = checker.checkInputs(argc, argv);
 
-    if (!inputsValids) { return 1; }
+    if (!inputsValids)
+    {
+        return 1;
+    }
 
     static Game gameOfLife(filePath, nbOfIterations, outputAllIterations);
 
+    const bool processReturn = gameOfLife.process();
+#ifdef ENABLE_CPU_PROFILING
+    ProfilerStop(); 
+#endif
+
+#ifdef ENABLE_HEAP_PROFILING
+    HeapProfilerDump("Heap profiling") ;
+    HeapProfilerStop();
+#endif
+
     // True==1 but 1 is an error return so return the opposite of the return
-    return !gameOfLife.process();
+    return !processReturn;
 }
