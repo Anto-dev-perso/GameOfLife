f7b2f494d0a52bfd138b2629cedbab5a8cfbbb92
AntoC <antonin.coupanec@gmail.com>
2024-09-22 20:27:32 +0200

    build: add a build script

---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Default build type
+BUILD_TYPE="Release"
+BUILD_DIR="build-release"
+
+# Parse command-line options
+usage() {
+    echo "ERROR in arguments. Please use one of the following: $0 [-c <Debug|Release|RelWithDebInfo> for the configuration you want for the binary] [-p <heap|cpu|none> for choosing the profiling binary you want] [-s for static analysis]"
+    exit 1
+}
+
+
+while getopts ":c:p:s" opt; do
+    case ${opt} in
+        c)
+            BUILD_TYPE=$OPTARG
+            ;;
+        s)
+            STATIC_ANALYSIS='-DCMAKE_EXPORT_COMPILE_COMMANDS=ON -D CMAKE_CXX_CLANG_TIDY="clang-tidy;-checks=*,-clang-analyzer-alpha.*"'
+            ;;
+        p)
+            PROFILING_TYPE="-DPROFILING_TYPE=$OPTARG"
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+
+if [ "$BUILD_TYPE" == "Debug" ]; then
+    BUILD_DIR="build-debug"
+elif [ "$BUILD_TYPE" == "RelWithDebInfo" ]; then
+    BUILD_DIR="build-relwithdebinfo"
+elif [ "$BUILD_TYPE" != "Release" ]; then
+    echo "Invalid build type: $BUILD_TYPE"
+    usage
+fi
+
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE $PROFILING_TYPE -Wno-dev .. 
+cmake --build . -- -j
